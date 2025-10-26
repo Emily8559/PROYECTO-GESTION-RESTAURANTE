009d83aa0a0a3b6f9719710cbc8dc88f41f38984
Emily paulina <emilysiste89@gmail.com>
2025-10-26 17:59:12 -0500

    Enumeración para {}
    Se comenta una enumeración para las {}, porque ayuda a evitar confusiones entre { ya que son muchas

--- Estructuras/Lista_enlazada.cs
+++ Estructuras/Lista_enlazada.cs
@@ -1,8 +1,36 @@
 namespace PRestaurante.Estructuras
-{
+{//1
     public class Nodo<T>
-    {
-        
+    {//2
         public T Valor { get; set; }
-    }
+        public Nodo<T>? Siguiente { get; set; }
+        public Nodo(T valor)
+        {//3
+            Valor = valor;
+            Siguiente = null;
+
+        }//3
+    }//2
+        public class Listaenlazada<T>
+    {//4
+        private Nodo<T>? cabeza;
+        public void Insertar(T valor)
+        {//5
+            Nodo<T> nuevo = new Nodo<T>(valor);
+            if (cabeza == null)
+                cabeza = nuevo;
+            else
+            {//6
+                Nodo<T> actual = cabeza;
+                while (actual.Siguiente != null)
+                    actual = actual.Siguiente;
+                actual.Siguiente = nuevo;
+                
+            }//6
+
+             
+        }//5
+    }//4
+
+    
 }
