ee225b20eb84668017e996cafe766f5694eed032
Emily paulina <emilysiste89@gmail.com>
2025-11-05 20:50:21 -0500

    Cambio
    Se quito los paréntesis que había puesto

--- Estructuras/pila.cs
+++ Estructuras/pila.cs
@@ -1,2 +1,11 @@
+namespace PRestaurante.Estructuras
+{//1
+    public class Pila<T>
+    {//2
+        private Nodo<T>? cima;
+        public void Apilar(T valor)
+        {//3
+            Nodo<T> nuevo = new Nodo<T>(valor);
             nuevo.Siguiente = cima;
+                throw new InvalidOperationException("La pila está vacía");
 }//1
