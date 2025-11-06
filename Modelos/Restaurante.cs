61b77e035bee2e62fbf0c3c6bee8d884d5039fbf
Emily paulina <emilysiste89@gmail.com>
2025-11-05 22:41:50 -0500

    Implementación de Tostring() y signo $
    Esto se uso con el fin de mostrar los datos del restaurante de forma mas ordenada, y el $ (interpolación), se uso para juntar texto y variable al mostrar la info. del restaurante

--- Modelos/Restaurante.cs
+++ Modelos/Restaurante.cs
@@ -1,2 +1,4 @@
         public string Nit { get; set; }
         public Restaurante(string nombre, string nit, string dueno, string direccion, string celular)
+        public override string ToString()
+            return $"Restaurante: {Nombre}, NIT: {Nit}, Dueño: {Dueno}, Dirección: {Direccion}, Celular: {Celular}";
