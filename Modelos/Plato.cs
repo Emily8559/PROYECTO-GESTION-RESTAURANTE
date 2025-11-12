3541c0feb78e2ef6ad6fdcc9fed732e7c375af0f
Emily paulina <emilysiste89@gmail.com>
2025-11-12 03:07:32 -0500

    Propiedad agregada
    Esta se agrego en la clase PlatoPedido usando el get para calcular el subtotal

--- Modelos/Plato.cs
+++ Modelos/Plato.cs
@@ -1,1 +1,23 @@
+namespace PRestaurante.Modelos
+{//1
     public class Plato
+    {//2
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+
+        public Plato(string codigo, string nombre, string descripcion, decimal precio)
+        {
+            Codigo = codigo;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Precio = precio;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nombre} - ${Precio} (Código): {Codigo})";
+        }
+    }//2
+}//1
---
+++ Modelos/PlatoPedido.cs
@@ -0,0 +1,1 @@
+    public decimal Subtotal
